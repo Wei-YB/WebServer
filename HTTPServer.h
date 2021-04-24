865503ce8355b4044b9e3a1993b47573ed71db4e
韦一波 <weiyibo97@gmail.com>
2021-04-24 13:17:44 +0800

    finish class tcp server

---
+++ HTTPServer.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "../TCPServer.h"
+#include "Parser.h"
+#include "Response.h"
+
+
+namespace
+NAMESPACE http {
+
+
+class HTTPServer {
+
+    public:
+
+
+private:
+    TcpServer server_;
+};
+
+}
